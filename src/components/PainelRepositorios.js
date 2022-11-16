5437fcca80ae4b815a0e0cc1b036e1d69e0e3442
harleymacedo <harley.ip@gmail.com>
2022-11-16 08:54:22 -0300

    Componente PainelRepositorio criado

---
+++ src/components/PainelRepositorios.js
@@ -0,0 +1,11 @@
+import React, {useState, useEffect} from 'react';
+
+const PainelRepositorio = () => {
+    return (
+        <div>
+            <h3>Componente PainelRepositorio</h3>
+        </div>
+    );
+}
+
+export default PainelRepositorio;
