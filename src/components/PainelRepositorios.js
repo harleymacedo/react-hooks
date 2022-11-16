97a96e75a9b2ff50ee1c1bf07c46e13afa5b2361
harleymacedo <harley.ip@gmail.com>
2022-11-16 09:01:09 -0300

    Arquivo env modificado

--- src/components/PainelRepositorios.js
+++ src/components/PainelRepositorios.js
@@ -3,7 +3,7 @@ import React, {useState, useEffect} from 'react';
 const PainelRepositorio = () => {
     return (
         <div>
-            <h3>Componente PainelRepositorio</h3>
+            <h3>Componente Painel Repositorio</h3>
         </div>
     );
 }
